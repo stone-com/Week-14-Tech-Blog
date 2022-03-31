4e4922de2131df454f94b98112b59b28abaece5a
Stone Comstock <comstockstone@yahoo.com>
2022-03-30 20:50:46 -0700

    post routes

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,56 @@
+const router = require('express').Router();
+const { Post } = require('../../models/');
+const withAuth = require('../../utils/auth');
+
+// create a new post
+router.post('/', withAuth, async (req, res) => {
+  const body = req.body;
+  console.log(body);
+  try {
+    const newPost = await Post.create({ ...body, userId: req.session.userId });
+    console.log('Success');
+    res.json(newPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// update a post
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const [affectedRows] = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (affectedRows > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// delete a post
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const [affectedRows] = Post.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (affectedRows > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
